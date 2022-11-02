7a2ae6220b37690a9a145e10dc0f68d692597145
gabrielbcruz99 <gabriel.cruz@al.forsoft.org.br>
2022-11-02 14:38:12 -0300

    Até o momento foram criadas as rotas da aplicação, o front-end, alguns controllers e o model de contato

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,3 @@
+<h3>Fornecedor</h3>
+
+
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\PrincipalCOntroller::class,'principal'])->name('site.index');
+
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::post('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function() {
+
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class,'index'])->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos-');
+
+});
+
