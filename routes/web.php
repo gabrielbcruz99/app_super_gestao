96d333146b7e156a848c8aa5cb940bcdaf84c596
gabrielbcruz99 <gabriel.cruz@al.forsoft.org.br>
2022-11-25 18:00:10 -0300

    Adição de novas views e rotas, implementação da funcionalidade de edição e listagem de registros.

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,18 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class,'index'])
     ->name('app.fornecedor');
 
+    Route::post('/fornecedor/listar', [\App\Http\Controllers\FornecedorController::class,'listar'])
+    ->name('app.fornecedor.listar');
+
+    Route::get('/fornecedor/adicionar', [\App\Http\Controllers\FornecedorController::class,'adicionar'])
+    ->name('app.fornecedor.adicionar');
+
+    Route::post('/fornecedor/adicionar', [\App\Http\Controllers\FornecedorController::class,'adicionar'])
+    ->name('app.fornecedor.adicionar');
+
+    Route::get('/fornecedor/editar/{id}/{msg?}', [\App\Http\Controllers\FornecedorController::class,'editar'])
+    ->name('app.fornecedor.editar');
+
     Route::get('/produto', [\App\Http\Controllers\ProdutoController::class,'index'])
     ->name('app.produto');
 
